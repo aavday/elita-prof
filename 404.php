426ca7085836d5854ef1a3b634bec6fe3b880b64
aavday <wrck7@yandex.ru>
2022-01-29 20:45:45 +0300

    Создана страница 404-ой ошибки

--- 404.php
+++ 404.php
@@ -1,5 +1,7 @@
-<?
+<?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/urlrewrite.php');
+
+/** @global CMain $APPLICATION */
 
 CHTTP::SetStatus("404 Not Found");
 @define("ERROR_404","Y");
@@ -7,14 +9,11 @@ CHTTP::SetStatus("404 Not Found");
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $APPLICATION->SetTitle("404 Not Found");
+?>
 
-$APPLICATION->IncludeComponent("bitrix:main.map", ".default", Array(
-	"LEVEL"	=>	"3",
-	"COL_NUM"	=>	"2",
-	"SHOW_DESCRIPTION"	=>	"Y",
-	"SET_TITLE"	=>	"Y",
-	"CACHE_TIME"	=>	"36000000"
-	)
-);
+<div class="container page-404 color-white text-center">
+    <p class="page-404__title">404</p>
+    <p class="fs-big">Кажется, такой страницы не существует. Попробуйте <a href="/" class="white-link text-decoration-underline">начать сначала</a></p>
+</div>
 
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php"); ?>
