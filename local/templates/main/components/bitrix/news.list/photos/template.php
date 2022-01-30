0525bcb855143ebea4c8fc49299cae1a5b174d52
aavday <wrck7@yandex.ru>
2022-01-30 14:12:36 +0300

    Добавил тень, округленность и паддинг картинкам в разделе "Фото"

--- local/templates/main/components/bitrix/news.list/photos/template.php
+++ local/templates/main/components/bitrix/news.list/photos/template.php
@@ -12,7 +12,7 @@ if (!empty($arResult['ITEMS'])):
             $this->AddDeleteAction($item['ID'], $item['DELETE_LINK'], CIBlock::GetArrayByID($item['IBLOCK_ID'], 'ELEMENT_DELETE'));
             ?>
             <a href="<?=$item['DETAIL_PAGE_URL']; ?>" class="col-4 photos-item text-center" id="<?=$item['ID']; ?>">
-                <img src="<?=$item['PREVIEW_PICTURE']['SRC']; ?>" alt="<?=$item['NAME']; ?>" class="photos-item__img img-height-300px w-100">
+                <img src="<?=$item['PREVIEW_PICTURE']['SRC']; ?>" alt="<?=$item['NAME']; ?>" class="photos-item__img img-height-300px w-100 p-4 rounded shadow">
                 <p><?=$item['NAME']; ?></p>
             </a>
         <?php endforeach; ?>
