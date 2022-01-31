9beb3caab20a8efa8ee80a87c6f23528b1ce2b07
aavday <wrck7@yandex.ru>
2022-01-31 10:25:57 +0300

    Добавлен SetTitle в component_epilog детальной страницы раздела "Фото"

---
+++ local/templates/main/components/bitrix/news.detail/photo/component_epilog.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+
+$APPLICATION->SetTitle('Элита-Проф - ' . $arResult['NAME']);
