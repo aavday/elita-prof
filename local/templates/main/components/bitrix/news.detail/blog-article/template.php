2de7fe95fb27d378445cff85b8ae7e04a90bbefe
Alexey Avdeev <wrck7@yandex.ru>
2022-02-18 11:20:34 +0300

    Подправил микроразметку статей блога

--- local/templates/main/components/bitrix/news.detail/blog-article/template.php
+++ local/templates/main/components/bitrix/news.detail/blog-article/template.php
@@ -12,17 +12,13 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
     <p>
         <i class="fas fa-calendar me-2" aria-hidden="true"></i><span><?=$arResult['DISPLAY_ACTIVE_FROM']; ?></span>
     </p>
-    <p class="blog-article__detail-text">
+    <div class="blog-article__detail-text py-3" itemprop="articleBody">
         <?=$arResult['DETAIL_TEXT']; ?>
-    </p>
+    </div>
     <time itemprop="datePublished" datetime="<?=$arResult['DISPLAY_ACTIVE_FROM']; ?>"></time>
     <span itemprop="author" class="d-none">ООО "Элита-Проф"</span>
-    <div itemprop="publisher" itemscope itemtype="http://schema.org/Organization">
-        <div itemprop="logo" itemscope itemtype="http://schema.org/ImageObject">
-            <img itemprop="url" src="/src/img/logo.svg" />
-        </div>
-        <meta itemprop="name" content="<?=$_SERVER['HTTP_HOST']; ?>">
-        <meta itemprop="url" content="https://<?=$_SERVER['HTTP_HOST']; ?>">
+    <div itemprop="publisher">
+        <meta itemprop="name" content="Элита-Проф">
     </div>
     <meta itemscope itemprop="mainEntityOfPage" itemType="http://schema.org/WebPage" itemid="https://<?=$_SERVER['HTTP_HOST']; ?><?=$arResult['DETAIL_PAGE_URL']; ?>" />
 </div>
